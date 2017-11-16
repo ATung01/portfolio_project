7ff06516398295be9a1943d8fc4dd9c49eabd6bf
github username <acc.tung@gmail.com>
2017-11-15 20:13:09 -0500

    added react-materialize

---
+++ src/components/Content.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Button, Icon} from 'react-materialize'
+
+
+export default class Content extends React.Component {
+
+  render(){
+    return(
+
+      <Button waves='light'>EDIT ME<Icon left>save</Icon></Button>
+    )
+  }
+}
